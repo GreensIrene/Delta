8757e1a5eb8a8cef30353540b5f404b530333d68
GreensIrene <albeenairene28@gmail.com>
2021-07-07 10:40:35 +0530

    Nisha Commit

---
+++ src/org/login/Login.java
@@ -0,0 +1,12 @@
+package org.login;
+
+public class Login {
+
+	public static void userName() {
+		System.out.println("Username");
+	}
+	
+	public static void main(String[] args) {
+		userName();
+	}
+}
